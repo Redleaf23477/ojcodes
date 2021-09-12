d9fc5ad0fbc797c3de2fc7646f90309d9394a2df
redleaf23477 <schpokeool@gmail.com>
2021-09-12 16:42:47 +0800

    2021 fb hacker cup round1

---
+++ fb/2021-round1/B.cpp
@@ -0,0 +1,85 @@
+// by redleaf23477
+#include <bits/stdc++.h>
+
+// iostream macros
+#define endl '\n'
+#define var(x) "[" << #x << "=" << x << "]"
+// variadic print template, requires c++17
+template<typename Sep, typename Head, typename... Tail>
+void fold_print(Sep sep, Head&& h, Tail&&... args) {
+    ((std::cerr << h) << ... << (std::cerr << sep, args));
+}
+#ifdef REDLEAF_LOCAL
+#define debug(...) do {                                     \
+    std::cerr << "[DEBUG:" << __LINE__ << "] ";             \
+    std::cerr << "(" << #__VA_ARGS__ ") = (";               \
+    fold_print(", ", __VA_ARGS__);                          \
+    std::cerr << ")\n";                                     \
+    std::cerr.flush();                                      \
+} while (0)
+#define info(...) do {                                      \
+    std::cerr << "[INFO:" << __LINE__ << "] ";              \
+    fold_print(" ", __VA_ARGS__);                           \
+    std::cerr << "\n";                                      \
+    std::cerr.flush();                                      \
+} while (0)
+#else
+#define debug(...) do { } while (0)
+#define info(...) do { } while (0)
+#endif
+// chrono macros
+#define chrono_now std::chrono::high_resolution_clock::now()
+#define chrono_duration(t1, t2) std::chrono::duration<double>(t2-t1).count()
+#define chrono_rand_seed chrono_now.time_since_epoch().count()
+// random
+std::mt19937_64 myrand(chrono_rand_seed);
+// stl
+#define range(seq) seq.begin(), seq.end()
+
+
+using namespace std;
+using LL = long long int;
+
+void init();
+void process();
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int T; cin >> T;
+    for (int t = 1; t <= T; t++) {
+        cout << "Case #" << t << ": ";
+        init();
+        process();
+    }
+    cout.flush();
+    return 0;
+}
+
+int R, C, A, B;
+
+void init() {
+    cin >> R >> C >> A >> B;
+}
+
+void process() {
+    if (A < R+C-1 || B < R+C-1) {
+        cout << "Impossible" << endl;
+        return;
+    }
+    cout << "Possible" << endl;
+    for (int r = 0; r < R-1; r++) {
+        for (int c = 0; c < C; c++) {
+            cout << 1 << " \n"[c==C-1];
+        }
+    }
+    for (int c = 0; c < C; c++) {
+        if (c == 0) {
+            cout << B - (R+C-2) << " ";
+        } else if (c == C-1) {
+            cout << A - (R+C-2) << endl;
+        } else {
+            cout << 1 << " ";
+        }
+    }
+}
+
